a0ba657330a29c1d74bd9fee76c71deaacfc416a
Richard Finn <finnr@oregonstate.edu>
2021-04-13 23:14:26 -0400

    Starting to create the Company model. Did not run makemigrations yet, as I'm going to wait to see if this model needs to be updated to contain a user foreign key

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+	INDUSTRY_CHOICES = [
+		('tech', 'Tech'),
+		('ecommerce', 'Ecommerce'), 
+		('healthcare', 'Healthcare'),
+		('pharma', 'Pharmaceutical'),
+		('aerospace', 'Aerospace'),
+		('fintech', 'FinTech'),
+		('nonprofit', 'Nonprofit'),
+		('defense', 'Defense'),
+		('other', 'Other'),
+	]
+
+	name = models.CharField(max_length=100)
+	careers_url = models.URLField(max_length=300, null=True)
+	industry = models.CharField(choices=INDUSTRY_CHOICES, max_length=100, null=True)
