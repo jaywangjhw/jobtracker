eb1b7a62958bf24dc0d48eedd76416f7ffba45d1
luke-nam <lukenamnam@gmail.com>
2021-04-17 19:42:35 -0700

    Created Position form to add to database

--- jobs/urls.py
+++ jobs/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='jobs-home'),
     path('companies/', views.companies, name='jobs-companies'),
-    path('positions/', views.positions, name='jobs-positions'),
+    path('positions/', views.list_positions, name='jobs-positions'),
+    path('positions/new', views.add_position, name='jobs-new-position')
 ]
