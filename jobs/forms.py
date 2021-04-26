c380d20aec92db863986196d2547954e5c50e6b1
jaywangjhw <55812968+jaywangjhw@users.noreply.github.com>
2021-04-26 01:43:37 -0700

    Added CRUD to contacts
    Added CRUD to contacts

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Position, Company, Account
+from .models import Position, Company, Account, Contact
 
 # Need to add skills to db
 
