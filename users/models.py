f0e340ef8decbf1f66385b4f59d7e60b74f275b6
jaywangjhw <55812968+jaywangjhw@users.noreply.github.com>
2021-05-02 01:06:31 -0700

    Added changes needed for user profile
    Added changes needed for user profile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+
