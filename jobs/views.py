fc0d736b903256c6531c5dfdcecfcbd697826fd4
Richard Finn <finnr@oregonstate.edu>
2021-05-02 16:39:44 -0400

    CRUD for communications

--- jobs/views.py
+++ jobs/views.py
@@ -6,7 +6,7 @@ from django.views.generic import ListView, CreateView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
-from .models import Position, Company, Account, Contact
+from .models import Position, Company, Account, Contact, Communication
 from .forms import PositionForm
 import json
 
@@ -191,6 +191,54 @@ class ContactDeleteView(LoginRequiredMixin, DeleteView):
     model = Contact
     success_url = reverse_lazy('jobs-contacts')
 
+#-------------------------------------Communication Views----------------------------------------
+
+class CommunicationListView(LoginRequiredMixin, ListView):
+    
+    model = Communication
+    template_name = 'jobs/communications.html'
+    context_object_name = 'comms'
+    fields = ['application', 'contact', 'date', 'method', 'notes']
+
+
+class CommunicationCreateView(LoginRequiredMixin, CreateView):
+    
+    model = Communication
+    template_name = 'jobs/add_communication.html'
+    context_object_name = 'comms'
+    fields = ['application', 'contact', 'date', 'method', 'notes']
+    success_url = reverse_lazy('jobs-contacts')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class CommunicationUpdateView(LoginRequiredMixin, UpdateView):
+    
+    model = Communication
+    fields = ['application', 'contact', 'date', 'method', 'notes']
+    template_name = 'jobs/update_communication.html'
+    context_object_name = 'comms'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class CommunicationDeleteView(LoginRequiredMixin, DeleteView):
+    
+    model = Communication
+    success_url = reverse_lazy('jobs-contacts')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Pass the pk as a context variable, so that our templtes can access it.
+        context['id'] = self.kwargs['pk']
+        return context
+
+
+#-------------------------------------Communication Views End-------------------------------------
 
 @login_required
 def account(request):
