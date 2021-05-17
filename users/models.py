631221d4ba056603ddc9b98f721ce77e0b77bf7d
jaywangjhw <55812968+jaywangjhw@users.noreply.github.com>
2021-05-16 19:23:55 -0700

    Updated views and models
    Updated views and models

--- users/models.py
+++ users/models.py
@@ -3,6 +3,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Document(models.Model):
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    upload = models.FileField()
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
