435ebcb58e33a96efec1b9d6fa025a2fa4fb4604
jaywangjhw <55812968+jaywangjhw@users.noreply.github.com>
2021-05-24 22:44:10 -0700

    Updated settings
    Updated settings

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -33,6 +33,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
+    'django.contrib.sites',   
+    'allauth',   
+    'allauth.account',   
+    'allauth.socialaccount',   
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -158,3 +163,23 @@ EMAIL_USE_TLS = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+AUTHENTICATION_BACKENDS = (
+ 'django.contrib.auth.backends.ModelBackend',
+ 'allauth.account.auth_backends.AuthenticationBackend',
+ )
+
+SITE_ID = 2
+LOGIN_REDIRECT_URL = '/'
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
