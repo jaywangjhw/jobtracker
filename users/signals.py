5bbe55c052e83b583e128788de6d6d2990220d3f
jaywangjhw <55812968+jaywangjhw@users.noreply.github.com>
2021-05-31 00:56:22 -0700

    Fixed signals issue
    Fixed signals issue

--- users/signals.py
+++ users/signals.py
@@ -2,13 +2,6 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
-
-
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from allauth.exceptions import ImmediateHttpResponse
@@ -21,16 +14,23 @@ from django.shortcuts import redirect
 from django.conf import settings
 import json
 
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
+
+
+# Source from https://stackoverflow.com/questions/24357907/django-allauth-facebook-redirects-to-signup-when-retrieved-email-matches-an-exis
 class MyLoginAccountAdapter(DefaultAccountAdapter):
     '''
-    Overrides allauth.account.adapter.DefaultAccountAdapter.ajax_response to avoid changing
-    the HTTP status_code to 400
+    Overrides allauth.account.adapter.DefaultAccountAdapter.ajax_response
     '''
 
     def get_login_redirect_url(self, request):
-        """ 
-        """
+        '''
+        Redirects url after authentication
+        '''
         if request.user.is_authenticated():
             return settings.LOGIN_REDIRECT_URL.format(
                 id=request.user.id)
@@ -40,21 +40,14 @@ class MyLoginAccountAdapter(DefaultAccountAdapter):
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     '''
-    Overrides allauth.socialaccount.adapter.DefaultSocialAccountAdapter.pre_social_login to 
-    perform some actions right after successful login
+    Overrides allauth.socialaccount.adapter.DefaultSocialAccountAdapter.pre_social_login
     '''
     def pre_social_login(self, request, sociallogin):
-        pass    # TODOFuture: To perform some actions right after successful login
+        pass
 
 @receiver(pre_social_login)
 def link_to_local_user(sender, request, sociallogin, **kwargs):
-    ''' Login and redirect
-    This is done in order to tackle the situation where user's email retrieved
-    from one provider is different from already existing email in the database
-    (e.g facebook and google both use same email-id). Specifically, this is done to
-    tackle following issues:
-    * https://github.com/pennersr/django-allauth/issues/215
-
+    ''' Fixes issue where user's email retrieved is different from existing email in the database
     '''
     email_address = sociallogin.account.extra_data['email']
     User = get_user_model()
