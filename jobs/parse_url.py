d3e78194f98d1bad4330bac2798cc7ce8b8aadf1
Richard Finn <finnr@oregonstate.edu>
2021-06-01 17:27:17 -0400

    Strip whitespace from company names when parsing job postings

--- jobs/parse_url.py
+++ jobs/parse_url.py
@@ -27,7 +27,8 @@ def get_linkedin_data(url):
 	data = {}
 
 	try:
-		data['company'] = soup.find('a', class_='topcard__org-name-link').text
+		company_name = soup.find('a', class_='topcard__org-name-link').text
+		data['company'] = company_name.strip()
 	except:
 		pass
 
@@ -72,7 +73,8 @@ def get_indeed_data(url):
 	data = {}
 
 	try:
-		data['company'] = soup.find('div', class_='icl-u-textColor--success').text
+		company_name = soup.find('div', class_='icl-u-textColor--success').text
+		data['company'] = company_name.strip()
 	except:
 		pass
 
